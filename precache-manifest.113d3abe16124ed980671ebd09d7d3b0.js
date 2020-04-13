e88b649993ba223783b9e1cd2b818335cbbf3df5
Vinay Koshy <vinyoshy2@gmail.com>
2020-04-13 18:33:32 -0500

    Updates

---
+++ precache-manifest.113d3abe16124ed980671ebd09d7d3b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d0da941c07083a71e89abbd3366a3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7b487286dde4bb29588",
+    "url": "/static/css/main.b0b638c3.chunk.css"
+  },
+  {
+    "revision": "c26f927a1bb8f0c8d3ab",
+    "url": "/static/js/2.35202ad0.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/static/js/2.35202ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b487286dde4bb29588",
+    "url": "/static/js/main.bb700c43.chunk.js"
+  },
+  {
+    "revision": "59595df8ebc9f7c01fe6",
+    "url": "/static/js/runtime-main.f4593c96.js"
+  },
+  {
+    "revision": "1b5d1158a2d21d9179cb2199853ee6e0",
+    "url": "/static/media/candidates.1b5d1158.png"
+  },
+  {
+    "revision": "c3c817520c3382ce64e4eb9a8e59229c",
+    "url": "/static/media/interface_screenie_highlight.c3c81752.png"
+  }
+]);
