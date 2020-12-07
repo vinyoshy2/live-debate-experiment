a94e84b8d7f9182300ea2ede4b33d25821729ccb
Vinay Koshy <vinyoshy2@gmail.com>
2020-12-07 11:25:37 -0600

    adjusted timing on comments.json

--- src/data/json/second_update.py
+++ src/data/json/second_update.py
@@ -1,9 +1,9 @@
 import json
 
 data = {}
-with open("comments_pro.json", "r") as f:
+with open("comments.json", "r") as f:
     data = json.load(f)
 for entry in data["entries"]:
     entry["activation_time"] = entry["activation_time"] - 10000
-with open("comments_pro_adjusted.json", "w+") as f2:
+with open("comments_adjusted.json", "w+") as f2:
     f2.write(json.dumps(data, indent=4))
